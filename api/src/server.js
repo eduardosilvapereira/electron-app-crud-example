4b452eeb19ff2fe651712a62e1df8e854100ccfb
Eduardo Pereira <silvapereiraeduardo@gmail.com>
2021-01-24 23:47:07 -0300

    Correct way to implement Express with Json Server

--- api/src/server.js
+++ api/src/server.js
@@ -1,12 +1,12 @@
+const express = require('express')
 const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('src/db.json')
-const middlewares = jsonServer.defaults()
 
 const PORT = process.env.PORT || 3001;
 
-server.use(middlewares)
-server.use(router)
+const server = express();
+const apiRoutes = jsonServer.router('src/db.json')
+
+server.use('/api',apiRoutes)
 server.listen(PORT, () => {
   console.log('The server is running on port ' + PORT)
 })
